7ff71f59ca0fa48956d16ddbdf36930799aa7b90
Matthew <105422393+mtrdata@users.noreply.github.com>
2024-08-01 12:18:46 +0000

    Update about_me.py

--- pages/about_me.py
+++ pages/about_me.py
@@ -6,7 +6,7 @@ from streamlit_extras.colored_header import colored_header
 #######################
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
-    st.image("./assets/profile_image.png", width=230)
+    st.image("./assets/logo.png", width=230)
 
 with col2:
     colored_header(
